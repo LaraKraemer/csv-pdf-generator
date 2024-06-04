8dd6fbd99ffad33ab7e8275bf760fd6d8107f400
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2024-06-04 17:00:28 +0200

    Add more pages for each topic

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     # set color
     pdf.set_text_color(250,140,300)
-    # create text variables
+    # create text variables & iterate over topic rows
     pdf.cell(w=0, h=12, txt=row["Topic"],
              align="L", ln=1)
     # add line under cell
     pdf.line(10, 21, 200, 21)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
 
 
 
