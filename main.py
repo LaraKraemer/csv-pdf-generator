0303c74fec84d6ae6a77bf4d6190711ce1028346
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2024-06-04 16:49:15 +0200

    Making data frame from csv and for loop

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
-# method to add pages
-pdf.add_page()
+df = pd.read_csv("topics.csv")
 
-# set font family
-pdf.set_font(family="Times", style="B", size=12)
-# create text variables
-pdf.cell(w=0, h=12, txt="Hello There",
-         align="L", ln=1)
+for index, row in df.iterrows():
+    # method to add pages
+    pdf.add_page()
+    # set font family
+    pdf.set_font(family="Times", style="B", size=24)
+    # set color
+    pdf.set_text_color(250,140,300)
+    # create text variables
+    pdf.cell(w=0, h=12, txt=row["Topic"],
+             align="L", ln=1)
 
-pdf.set_font(family="Times", size=10)
-pdf.cell(w=0, h=12, txt="Hi There",
-         align="L", ln=1)
-
-
-pdf.add_page()
-
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello There",
-         align="L", ln=1)
-
-pdf.set_font(family="Times", size=10)
-pdf.cell(w=0, h=12, txt="Hi There",
-         align="L", ln=1)
 
 # create pdf file called output.pdf
 pdf.output("output.pdf")
