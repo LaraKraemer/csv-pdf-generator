0487d589f60e7e376f211394094f6a2282b89271
LaraKraemer <51157020+LaraKraemer@users.noreply.github.com>
2024-06-06 14:05:18 +0200

    Correct filepath

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 
-df = pd.read_csv("topics.csv")
+df = pd.read_csv("src/topics.csv")
 
 for index, row in df.iterrows():
     # method to add pages
@@ -19,7 +19,6 @@ for index, row in df.iterrows():
     # add horizontal lines
     for line in range(20, 298, 10):
         pdf.line(10, line, 200,line)
-
 
     # set footer to pages
     pdf.ln(265)
